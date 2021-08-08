3ec8e013130112e9e31b5830b5995f3d3aea949d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2021-08-08 12:44:23 +0200

    add test ability

--- tests/MotorcycleGraph.test.ts
+++ tests/MotorcycleGraph.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals';
-import { MotorcyclePoint } from './../src/Motorcycle/MotorcyclePoint';
+import { MotorcycleGraph } from './../src/Motorcycle/MotorcycleGraph';
 
-test("hello world test", () => {
+test("MotorcyclePoint", () => {
   let point = new MotorcyclePoint(0, 0);
 
   expect(point.time).toBe(0);
